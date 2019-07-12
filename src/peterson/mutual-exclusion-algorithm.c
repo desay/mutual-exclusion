15d2ad744c20fdf5a60f1a1d19269368603c3975
Helga Pavluchenko <olga@pavluchenko.com>
2019-07-12 14:12:52 +0300

    initial commit

---
+++ src/peterson/mutual-exclusion-algorithm.c
@@ -0,0 +1,35 @@
+//
+// Created by Helga on 2019-07-12.
+//
+
+// for compile use:
+// gcc -mutual-exclusion-algorithm.c -o mutual-exclusion-algorithm
+
+#include <stdio.h>
+#include <pthread.h>
+#include"threads.h"
+
+int flag[2];
+int turn;
+const int MAX = 1e9;
+int ans = 0;
+
+void lock_init() {
+
+}
+
+void lock(int self) {
+
+}
+
+void unlock(int self) {
+
+}
+
+void* func(void *s) {
+
+}
+
+int main() {
+
+}
