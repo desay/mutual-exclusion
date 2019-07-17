d7cb748cd0ca25c6b82aeccdf5bb3f26e80cc6fe
Helga Pavluchenko <olga@pavluchenko.com>
2019-07-17 21:36:19 +0300

    lamport init cmt

---
+++ src/lamport/mutual-exclusion-algorithm.c
@@ -0,0 +1,14 @@
+//
+// Created by Helga on 2019-07-17.
+//
+
+// for compile use:
+// gcc -mutual-exclusion-algorithm.c -o mutual-exclusion-algorithm
+
+#include <stdio.h>
+#include <pthread.h>
+
+int main() {
+    
+    return 0;
+}
